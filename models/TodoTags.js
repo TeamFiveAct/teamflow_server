c0950fdd6aab28b1af014479b5fdc8667586750c
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-02-14 12:25:34 +0900

    feat : Todo와 Tag사이의 중간테이블 생성

---
+++ models/TodoTags.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const TodoTag = sequelize.define(
+    "TodoTag", 
+    {
+    todo_id: {
+        type: DataTypes.INTEGER,
+        references: { model: Todo, key: 'todo_id' },
+        onDelete: 'CASCADE',
+        primaryKey: true 
+      },
+      tag_id: {
+        type: DataTypes.INTEGER,
+        references: { model: Tag, key: 'tag_id' },
+        onDelete: 'CASCADE',
+        primaryKey: true 
+      },
+    }
+    {
+        tableName : "TodoTags",
+        timestamps : false,
+    }
+);
+module.exports = TodoTags;
