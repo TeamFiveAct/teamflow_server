df39bd857387a07ad55ce49969f61409ac6e94db
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-02-14 12:26:15 +0900

    fix : Todotags 중간테이블 코드 오류 수정

--- models/TodoTags.js
+++ models/TodoTags.js
@@ -2,24 +2,24 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
 const TodoTag = sequelize.define(
-    "TodoTag", 
-    {
+  "TodoTag",
+  {
     todo_id: {
-        type: DataTypes.INTEGER,
-        references: { model: Todo, key: 'todo_id' },
-        onDelete: 'CASCADE',
-        primaryKey: true 
-      },
-      tag_id: {
-        type: DataTypes.INTEGER,
-        references: { model: Tag, key: 'tag_id' },
-        onDelete: 'CASCADE',
-        primaryKey: true 
-      },
-    }
-    {
-        tableName : "TodoTags",
-        timestamps : false,
-    }
+      type: DataTypes.INTEGER,
+      references: { model: Todo, key: "todo_id" },
+      onDelete: "CASCADE",
+      primaryKey: true,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: { model: Tag, key: "tag_id" },
+      onDelete: "CASCADE",
+      primaryKey: true,
+    },
+  },
+  {
+    tableName: "TodoTags",
+    timestamps: false,
+  }
 );
 module.exports = TodoTags;
