d58b4c71d7b930c457b80e6bcf546e9cdc01fd2d
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-02-14 12:20:59 +0900

    feat : Todo  models에 tag_id 제거

--- models/Todo.js
+++ models/Todo.js
@@ -40,15 +40,6 @@ const Todo = sequelize.define(
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    /**
-     * tag_id를 배열로 저장한다고 했지만, MySQL에서는 ENUM/SET/JSON 등을 활용해야 합니다.
-     * 다대다 관계로 연결하는 경우에는 별도 중간 테이블(todo_tags) 등을 사용합니다.
-     * 아래 예시는 JSON으로 간단히 저장하는 경우 예시
-     */
-    // tag_ids: {
-    //   type: DataTypes.JSON, // 혹은 TEXT로 하고 GET/SET을 통해 JSON 파싱
-    //   allowNull: true,
-    // },
   },
   {
     tableName: "todos",
