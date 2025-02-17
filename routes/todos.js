4395abe51f6b354d462a0df92d5c60ff5b1e2a09
GUY BIN <heimishs@naver.com>
2025-02-17 10:11:19 +0900

    feat:라우터 수정 삭제,변경 코드추가 (#8)
    Co-authored-by: “heimishs” <“heimishs@naver.com”>

--- routes/todos.js
+++ routes/todos.js
@@ -1,12 +1,23 @@
 const express = require("express");
+const router = express.Router({ mergeParams: true }); // 👈 이 부분 추가!
 const controller = require("../controllers/Ctodos");
-const router = express.Router();
 
 // todos 라우터의 기본 URL은 todos/ 입니다!!!
 /* 컨트롤러의 이름은 임의로 설정하였으니 각각 용도에 맞춰 작성해주세요~ 😀 */
+
+// 전체 업무 리스트 조회
 router.post("/", controller.postTodoList);
+// 업무 생성
 router.post("/add", controller.postTodoCreate);
-router.post("/:todo_id", controller.postTodo);
+// 특정 업무 조회
+router.post("/view/:todo_id", controller.postTodo);
+// 업무 수정
+router.patch("/:todo_id", controller.patchTodo);
+// 업무 삭제
+router.delete("/:todo_id", controller.deleteTodo);
+// 업무 상태 변경
+router.patch("/state/:todo_id", controller.patchTodoState);
 
 
 module.exports = router;
+
