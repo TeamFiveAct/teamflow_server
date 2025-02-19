5f2ef6bfbd24085c311009827c8c44a3ca787a55
heimishs <heimishs@naver.com>
2025-02-19 12:25:01 +0900

    feat:이메일 초대 컨트롤러 작성중, 이메일 미들웨어 추가

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 // const env = process.env.NODE_ENV || 'development';
 //const env = 'production';
-const env = 'development';
+const env = 'localDev';
 const config = require(__dirname + '/../config/config.json')[env];
 
 const sequelize = new Sequelize(
