a7fbd1328a34a74c3057ab4ae0ee1a45271adf82
“ravegirl” <kyj002002@naver.com>
2025-02-21 12:30:50 +0900

    스웨거

--- routes/user.js
+++ routes/user.js
@@ -89,8 +89,76 @@ router.get('/session', controller.getSession);
 // 회원탈퇴
 router.delete('/', controller.deleteMyInfo);
 
-//사용자정보조회
+/**
+ * @swagger
+ * /v1/user/info:
+ *   get:
+ *     summary: 로그인 쿠키로 사용자 정보 조회
+ *     description: 로그인 쿠키를 통해 인증된 사용자의 정보를 조회합니다.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: cookie
+ *         name: cookie
+ *         required: true
+ *         description: 사용자 인증 쿠키
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: 사용자 정보가 성공적으로 조회됨.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 email:
+ *                   type: string
+ *                 nickname:
+ *                   type: string
+ *                 profile_image:
+ *                   type: string
+ */
 router.get('/info', controller.getUserInfo);
-//사용자정보수정
+
+/**
+ * @swagger
+ * /v1/user/info:
+ *   put:
+ *     summary: 로그인 쿠키로 사용자 정보 수정
+ *     description: 로그인 쿠키를 통해 인증된 사용자의 이메일, 닉네임, 프로필 이미지, 비밀번호 등 정보를 수정합니다.
+ *     tags:
+ *       - User
+ *     parameters:
+ *       - in: cookie
+ *         name: cookie
+ *         required: true
+ *         description: 사용자 인증 쿠키
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: newemail@example.com
+ *               nickname:
+ *                 type: string
+ *                 example: newnickname
+ *               profile_image:
+ *                 type: string
+ *                 example: http://example.com/new-profile.png
+ *               password:
+ *                 type: string
+ *                 example: newpassword
+ *     responses:
+ *       200:
+ *         description: 사용자 정보가 성공적으로 수정됨.
+ */
 router.put('/info', controller.updateUserInfo);
+
 module.exports = router;
--- routes/workspace.js
+++ routes/workspace.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const controller = require("../controllers/Cworkspace");
+const express = require('express');
+const controller = require('../controllers/Cworkspace');
 const router = express.Router({ mergeParams: true });
 const isAuthenticated = require('../middlewares/isAuthenticated'); // 로그인 여부 체크 미들웨어
 
@@ -7,22 +7,21 @@ const isAuthenticated = require('../middlewares/isAuthenticated'); // 로그인 
 /* 컨트롤러의 이름은 임의로 설정하였으니 각각 용도에 맞춰 작성해주세요~ 😀 */
 
 // 워크스페이스 생성
-router.post("/", isAuthenticated, controller.postSpaceCreate);
+router.post('/', isAuthenticated, controller.postSpaceCreate);
 
 // 현재 사용자가 참여한 모든 워크스페이스 조회 (GET)
-router.get("/user", isAuthenticated, controller.getMySpace);
+router.get('/user', isAuthenticated, controller.getMySpace);
 
-// 워크스페이스 초대 
-router.post("/invite", isAuthenticated, controller.postSpaceInvite);
+// 워크스페이스 초대
+router.post('/invite', isAuthenticated, controller.postSpaceInvite);
 
 // 워크스페이스 참여 신청 (초대 코드 입력 후 참여)
-router.post("/join", isAuthenticated, controller.postSpaceJoin);
+router.post('/join', isAuthenticated, controller.postSpaceJoin);
 
-// 특정 워크스페이스 조회 
-router.get("/:space_id", isAuthenticated, controller.getSpace);
+// 특정 워크스페이스 조회
+router.get('/:space_id', isAuthenticated, controller.getSpace);
 
-// 특정 워크스페이스 멤버 조회 
-router.get("/:space_id/member", isAuthenticated, controller.getSpaceMember);
-
+// 특정 워크스페이스 멤버 조회
+router.get('/:space_id/member', isAuthenticated, controller.postSpaceMember);
 
 module.exports = router;
