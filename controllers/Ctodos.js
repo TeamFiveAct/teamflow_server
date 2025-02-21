0c3b7eb0404026ce773693a212c06dd6ba312517
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-02-21 11:20:21 +0900

    refactor : todo 응답 공통 함수 적용

--- controllers/Ctodos.js
+++ controllers/Ctodos.js
@@ -1,5 +1,5 @@
-
-const todoModel = require("../models/Todo");
+const todoModel = require('../models/Todo');
+const responseUtil = require('../utils/ResponseUtil');
 // const { errorlogs } = require("../utils/common");
 
 // 전체 업무 리스트
@@ -9,22 +9,14 @@ exports.postTodoList = async (req, res) => {
     const todos = await todoModel.findAll({
       where: {
         space_id: req.params.space_id,
-      }
+      },
     });
     const allTodo = todos.map((todo) => todo.dataValues);
 
-    res.json({
-      status: "SUCCESS",
-      message: "전체 업무 조회성공",
-      data: allTodo,
-    });
+    res.send(responseUtil('SUCCESS', '전체 업무 조회성공했습니다.', allTodo));
   } catch (error) {
-    console.log("getTodoItem Controller Err:", error);
-    res.status(500).json({
-      status: "ERROR",
-      message: "전체 업무조회에 실패하였습니다.",
-      data: {},
-    });
+    console.log('getTodoItem Controller Err:', error);
+    res.send(responseUtil('ERROR', '전체 업무조회에 실패하였습니다.', null));
   }
 };
 
@@ -35,22 +27,17 @@ exports.postTodo = async (req, res) => {
     const todo = await todoModel.findOne({
       where: {
         space_id: space_id,
-        todo_id: todo_id
+        todo_id: todo_id,
       },
     });
-
-    res.json({
-      status: "SUCCESS",
-      message: "특정업무 조회성공",
-      data: { ...todo.dataValues },
-    });
+    res.send(
+      responseUtil('SUCCESS', '특정업무 조회성공에 성공했습니다.', {
+        ...todo.dataValues,
+      })
+    );
   } catch (error) {
-    console.log("getTodoItem Controller Err:", error);
-    res.status(500).json({
-      status: "ERROR",
-      message: "업무 조회에 실패하였습니다.",
-      data: {},
-    });
+    console.log('getTodoItem Controller Err:', error);
+    res.send(responseUtil('ERROR', '업무 조회에 실패하였습니다.', null));
   }
 };
 
@@ -65,18 +52,9 @@ exports.postTodoCreate = async (req, res) => {
       start_date: req.body.start_date,
       due_date: req.body.due_date,
     });
-
-    res.send({
-      status: "SUCCESS",
-      message: "업무생성 성공",
-      data: null,
-    });
+    res.send(responseUtil('SUCCESS', '업무생성 성공했습니다.', null));
   } catch (err) {
-    res.send({
-      status: "ERROR",
-      message: "업무 생성에 실패하였습니다.",
-      data: null,
-    });
+    res.send(responseUtil('ERROR', '업무 생성에 실패하였습니다.', null));
   }
 };
 
@@ -89,29 +67,18 @@ exports.patchTodo = async (req, res) => {
     // 해당 업무 찾기
     const todo = await todoModel.findByPk(todo_id);
     if (!todo) {
-      return res.status(404).send({
-        status: "ERROR",
-        message: "해당 업무를 찾을 수 없습니다.",
-        data: null,
-      });
+      return res.send(
+        responseUtil('ERROR', '해당 업무를 찾을 수 없습니다.', null)
+      );
     }
 
-    // 업무 데이터 업데이트 
+    // 업무 데이터 업데이트
     // body 로 들어온 값만 update 됨 (기존 조회했던 데이터 기준으로 수정)
     await todo.update(updateData);
-
-    res.send({
-      status: "SUCCESS",
-      message: "업무 수정 성공",
-      data: todo, // 수정된 데이터 반환
-    });
+    res.send(responseUtil('SUCCESS', '업무 수정 성공했습니다.', todo));
   } catch (err) {
     console.error(err);
-    res.status(500).send({
-      status: "ERROR",
-      message: "업무 수정에 실패하였습니다.",
-      data: null,
-    });
+    res.send(responseUtil('ERROR', '업무 수정에 실패하였습니다.', null));
   }
 };
 
@@ -123,28 +90,17 @@ exports.deleteTodo = async (req, res) => {
     // 삭제 전 삭제할 데이터가 존재하는지 확인인
     const todo = await todoModel.findByPk(todo_id);
     if (!todo) {
-      return res.status(404).send({
-        status: "ERROR",
-        message: "해당 업무를 찾을 수 없습니다.",
-        data: null,
-      });
+      return res.send(
+        responseUtil('ERROR', '해당 업무를 찾을 수 없습니다.', null)
+      );
     }
 
     // 해당 데이터 삭제 (기존 조회했던 데이터 기준으로 삭제)
     await todo.destroy();
-
-    res.send({
-      status: "SUCCESS",
-      message: "업무 삭제 성공",
-      data: null,
-    });
+    res.send(responseUtil('SUCCESS', '업무 삭제 성공했습니다.', null));
   } catch (err) {
     console.error(err);
-    res.status(500).send({
-      status: "ERROR",
-      message: "업무 삭제에 실패하였습니다.",
-      data: null,
-    });
+    res.send(responseUtil('ERROR', '업무 삭제에 실패하였습니다.', null));
   }
 };
 
@@ -157,28 +113,21 @@ exports.patchTodoState = async (req, res) => {
     // 1️⃣ 해당 업무 찾기
     const todo = await todoModel.findByPk(todo_id);
     if (!todo) {
-      return res.status(404).send({
-        status: "ERROR",
-        message: "해당 업무를 찾을 수 없습니다.",
-        data: null,
-      });
+      return res.send(
+        responseUtil('ERROR', '해당 업무를 찾을 수 없습니다.', null)
+      );
     }
 
     // 2️⃣ 상태 업데이트
     await todo.update({ state });
-
-    res.send({
-      status: "SUCCESS",
-      message: "업무 상태가 변경되었습니다.",
-      data: { id: todo.id, state: todo.state },
-    });
+    res.sed(
+      responseUtil('SUCCESS', '업무 상태가 변경되었습니다.', {
+        id: todo.id,
+        state: todo.state,
+      })
+    );
   } catch (err) {
     console.error(err);
-    res.status(500).send({
-      status: "ERROR",
-      message: "업무 상태 변경에 실패하였습니다.",
-      data: null,
-    });
+    res.send(responseUtil('ERROR', '업무 상태 변경에 실패하였습니다.', null));
   }
 };
-
