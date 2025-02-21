68ccc1d42658e1d27f11a7f1a06592aeac4ddff8
“ravegirl” <kyj002002@naver.com>
2025-02-21 10:40:07 +0900

    라우터 수정

--- models/index.js
+++ models/index.js
@@ -115,7 +115,7 @@ PasswordReset.belongsTo(User, {
 });
 
 // 데이터베이스 동기화
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   console.log('Database synced!');
 });
 
