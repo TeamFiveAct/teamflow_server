4df8075e7bbf399c9fb3c289e7f32a7c8bd685f6
GUY BIN <heimishs@naver.com>
2025-02-23 13:02:40 +0900

    Feat/email (#39)
    * fext:업무 소프트딜리트 하드 딜리트 기능 개발완료
    
    * feat:이메일 초대기능 구현
    
    ---------
    
    Co-authored-by: “heimishs” <“heimishs@naver.com”>

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,18 +1,11 @@
 const isAuthenticated = (req, res, next) => {
-  console.log('🔍 [미들웨어 실행] isAuthenticated 호출됨');
-  console.log('✅ 세션 정보:', req.session);
-  console.log('✅ Passport 정보:', req.session.passport);
-  console.log('✅ isAuthenticated 결과:', req.isAuthenticated());
-
   if (!req.isAuthenticated()) {
-      console.log('로그인 필요!');
       return res.status(401).json({
           status: 'ERROR',
           message: '로그인이 필요합니다.',
           data: null,
       });
   }
-  console.log('로그인 확인됨');
   next();
 };
 
