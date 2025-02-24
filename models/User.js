d910ca2a8965a0c75505871562f5afd7981b28ac
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-02-24 10:55:18 +0900

    feat : 회원탈퇴 soft delete 위한 user 모델에 컬럼 추가

--- models/User.js
+++ models/User.js
@@ -44,6 +44,10 @@ const User = sequelize.define(
       type: DataTypes.STRING(500),
       allowNull: true,
     },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
     created_at: {
       type: DataTypes.DATE,
       allowNull: false,
@@ -53,7 +57,7 @@ const User = sequelize.define(
   {
     tableName: 'users',
     timestamps: false, // created_at 등을 직접 다룬다면 true일 필요가 없을 수 있습니다.
-    
+    paranoid: true,
   }
 );
 
