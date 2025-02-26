a065daa5086780194cd80b23a4ecb1cd57a76459
GUY BIN <heimishs@naver.com>
2025-02-26 14:22:47 +0900

    Fix/todo (#62)
    * fix:워크스페이스 참여자 전체 조회 버그수정
    
    * fix:워크스페이스 참여자 프로필 사진 가져오기 추가
    
    * fix:특정 업무 조회시 유저인증이 안되는 문제 수정

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -2,7 +2,7 @@ const isAuthenticated = (req, res, next) => {
   if (!req.isAuthenticated()) {
       return res.status(401).json({
           status: 'ERROR',
-          message: '로그인이 필요합니다.',
+          message: '로그인되지 않은 사용자입니다.',
           data: null,
       });
   }
